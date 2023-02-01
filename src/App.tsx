1c26bba04852aeca352199951344276724a28cb9
AnthonyHale248 <42832023+AnthonyHale248@users.noreply.github.com>
2023-02-01 23:28:02 +0200

    added datepicker for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import Header from "./Header/Header";
 import Dashboard from "./Dashboard/Dashboard";
 import Calendar from "./Calendar/Calendar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Container>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/calendar" element={<Calendar />} />
-          </Routes>
-        </Container>
-      </div>
-    </BrowserRouter>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <BrowserRouter>
+        <div className="App">
+          <Container>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </Container>
+        </div>
+      </BrowserRouter>
+    </LocalizationProvider>
   );
 };
 
--- src/Dashboard/SidebarDatepicker/SidebarDatepicker.tsx
+++ src/Dashboard/SidebarDatepicker/SidebarDatepicker.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+
+import TextField from "@mui/material/TextField";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 
 import "./SidebarDatepicker.css";
 
 const UserInfo: FC = () => {
+  const [value, setValue] = useState<Date | null>(new Date());
+
   return (
     <>
-      <div className="right-sidebar__datepicker"></div>
+      <div className="right-sidebar__datepicker">
+        <StaticDatePicker
+          orientation="portrait"
+          openTo="day"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </div>
       <div className="button">Add Event</div>
     </>
   );
