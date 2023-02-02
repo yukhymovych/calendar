884e64c2c1ec0eab5739e39fbd5fd3f31d66809a
AnthonyHale248 <42832023+AnthonyHale248@users.noreply.github.com>
2023-02-03 01:15:27 +0200

    added library fullcalendar to Calendar page

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,8 +1,31 @@
 import React, { FC } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css";
 
 const Calendar: FC = () => {
-  return <>Calendar</>;
+  const handleClick = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+        initialView="dayGridMonth" //dayGridMonth, timeGridWeek, listWeek, dayGridWeek
+        dateClick={handleClick}
+        eventClick={handleClick}
+        editable={true}
+        events={[
+          { title: "event 1", date: "2023-02-01" },
+          { title: "event 2", date: "2023-02-02" },
+        ]}
+      />
+    </>
+  );
 };
 
 export default Calendar;
