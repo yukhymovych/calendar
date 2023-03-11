d1dae2ba28ff33bf553099025f57c9c585b8e108
AnthonyHale248 <42832023+AnthonyHale248@users.noreply.github.com>
2023-03-11 22:29:35 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // extends: 'eslint:recommended',
+  // rules: {
+  //   // enable additional rules
+  //   indent: ['error', 4],
+  //   'linebreak-style': ['error', 'unix'],
+  //   semi: ['error', 'always'],
+
+  //   // override configuration set by extending "eslint:recommended"
+  //   'no-empty': 'warn',
+  //   'no-cond-assign': ['error', 'always'],
+
+  //   // disable rules from base configurations
+  //   'for-direction': 'off',
+  // },
+};
